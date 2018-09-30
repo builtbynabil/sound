b2b7aca0fc079dfef26808391d98cf106f9eb305
Nabil Ridhwanshah <35335428+builtbynabil@users.noreply.github.com>
2018-09-30 09:44:13 +0800

    updated again to test and debug

--- main.js
+++ main.js
@@ -1,40 +1,40 @@
 let video;
 
-function setup(){
-    createCanvas(500,500,WEBGL)
+function setup() {
+    createCanvas(500, 500, WEBGL)
     video = createCapture(VIDEO)
-    video.size(200,200)
+    video.size(200, 200)
     video.elt.setAttribute('playsinline', '');
 }
 
-function draw(){
-    
-    let mx = mouseX -width / 2;
+function draw() {
+
+    let mx = mouseX - width / 2;
     let my = mouseY - height / 2
-    
-    background(10)
-    
+
+    background(255)
+
     // stroke(255)
     noStroke();
-    directionalLight(255,255,255 , 0 , frameCount*0.05 , 0)
-    
+    directionalLight(255, 255, 255, 0, 200, 0)
+
     // directionalLight(255,255,255 , 0 , 0 , 200)
-    
+
     // translate(mx,my)
-    
+
     // rotateX(frameCount * 0.01)
     // rotateY(frameCount * 0.001)
     specularMaterial(255);
-    
+
     // torus(i, 10);
     rotateY(mouseY * 0.02);
     rotateX(mouseX * 0.02);
-    
-        
-        torus(60 , 10)
 
-    specularMaterial(0,255,255);
+
+    torus(60, 10)
+
+    specularMaterial(0, 255, 255);
     rotateX(frameCount * 0.05)
     rotateY(frameCount * 0.1)
-    box(50 , 50 , 50);
+    box(50, 50, 50);
 }
