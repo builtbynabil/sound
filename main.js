e0203436a60c88a58c7c4c45f4e88efc00d051e0
Nabil Ridhwanshah <35335428+builtbynabil@users.noreply.github.com>
2018-09-30 11:22:11 +0800

    music-visualization

--- main.js
+++ main.js
@@ -1,36 +1,101 @@
-let video;
+let canvas;
+
+function preload() {
+    song = loadSound("./song.mp3")
+
+}
+
 
 function setup() {
-    createCanvas(windowWidth, windowHeight, WEBGL)
-    video = createCapture(VIDEO)
-    video.size(200, 200)
-    video.elt.setAttribute('playsinline', '');
-    video.hide()
+    canvas = createCanvas(windowWidth, windowHeight, WEBGL)
+
+    amplitude = new p5.Amplitude();
+    // canvas.style('z-index' , '-1')
+    song.play();
+
+    pause = select(".pause")
+    play = select(".play")
+
+    // song.jump(slider.value())
 }
 
 function draw() {
+    
+    var radius = width * 1.5;
 
-    let mx = mouseX - width / 2;
-    let my = mouseY - height / 2
-
+    orbitControl();
     background(0)
 
-    // stroke(255)
-    noStroke();
-    // directionalLight(255, 255, 255, 0, 200, 0)
+    directionalLight(0, 0, 0, 0, 200, 0)
 
-    // directionalLight(255,255,255 , 0 , 0 , 200)
+    // ROTATING EVERYTHING
+    rtt = frameCount * 0.005
+    rotateX(rtt)
+    rotateY(rtt)
+    rotateZ(rtt)
 
-    // translate(mx,my)
+    normalMaterial()
+    boxsize = 250 + frameCount * -1;
+    // boxsize = 50;
 
-    // rotateX(frameCount * 0.01)
-    // rotateY(frameCount * 0.001)
-    specularMaterial(255);
+    var level = amplitude.getLevel();
+    boxsize = level * 200
+
+    push()
+
+    newrtt = frameCount * 0.01;
     
-    specularMaterial(0, 255, 255);
-    texture(video)
-    rotateX(frameCount * 0.001)
-    rotateY(frameCount * 0.05)
-    rotateZ(frameCount * 0.05)
-    box(200,200,200);
+    rotateX(newrtt)
+    rotateY(newrtt)
+    rotateZ(newrtt)
+
+    box(boxsize, boxsize, boxsize)
+
+    pop()
+
+    push()
+
+    newrtt = frameCount * 0.01;
+
+    rotateX(newrtt)
+    rotateY(newrtt)
+    rotateZ(newrtt)
+
+    translate(300 , 0)
+    sphere(boxsize)
+    pop()
+
+    push()
+    translate(-300 , 0)
+    sphere(boxsize)
+    pop()
+
+    for (var i = 0; i <= 12; i++) {
+        for (var j = 0; j <= 12; j++) {
+            push();
+            var a = j / 12 * PI;
+            var b = i / 12 * PI;
+            translate(sin(2 * a) * radius * sin(b), cos(b) * radius / 2, cos(2 * a) * radius * sin(b));
+            if (j % 2 === 0) {
+                cone(30, 30);
+            } else {
+                box(30, 30, 30);
+            }
+            pop();
+        }
+    }
+    
+    pause.mousePressed( function(){
+
+        if(song.isPlaying()){
+            song.pause()
+            pause.html("Play")
+        }else{
+            song.play()
+            pause.html("Pause")
+        }
+        
+    } )
+
+
 }
